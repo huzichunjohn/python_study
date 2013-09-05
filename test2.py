4a06880e400747bdc4582247df157937559a549e
johnhu <huzichunjohn@126.com>
2013-09-05 11:45:11 +0800

    add test2.py

---
+++ test2.py
@@ -0,0 +1,22 @@
+#!/bin/env python
+import time
+import thread
+
+count = 0
+lock = thread.allocate_lock()
+def test():
+    global count, lock
+    lock.acquire()
+    for i in range(0,10000):
+	count += 1
+    lock.release() 
+
+for i in range(0,10):
+    thread.start_new_thread(test,())
+
+time.sleep(5)
+print count
+    
+
+
+
