4c0b6d32faa1311c63e2933ba22fac8c31c5c25b
johnhu <huzichunjohn@126.com>
2013-09-26 17:08:38 +0800

    Add closure.py

---
+++ closure.py
@@ -0,0 +1,16 @@
+#!/bin/env python
+def outer(arg):
+    print "outer(%s)" % arg
+
+    def inner(val):
+	print "inner(%s) (arg=%s)" % (val, arg)
+	return val * arg
+
+    return inner
+
+print "Creating thrice:"
+thrice = outer(3)
+
+print "\nCalling thrice:"
+print thrice(3)
+print thrice('a')
